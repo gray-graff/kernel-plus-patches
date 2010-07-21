34d6a1c2b79370d1e8f1772ceadcd536d0c70c7e
Aeliya Grevnyov <gray_graff@altlinux.org>
2010-07-22 01:18:37 +0800

    bfs-318

--- kernel/sched.c
+++ kernel/sched.c
@@ -1,3 +1,6 @@
+#ifdef CONFIG_SCHED_BFS
+#include "sched_bfs.c"
+#else
 /*
  *  kernel/sched.c
  *
@@ -10963,3 +10966,4 @@ void synchronize_sched_expedited(void)
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
 
 #endif /* #else #ifndef CONFIG_SMP */
+#endif /* CONFIG_SCHED_BFS */
